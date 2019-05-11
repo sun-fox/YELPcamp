e7cffe1bb514d51d748a0a299163c269944751f0
SUNNY KUMAR SINGH <39754412+sun-fox@users.noreply.github.com>
2019-05-11 20:45:27 +0530

    YelpCamp
    A Sample e-camp booking website

---
+++ YelpCamp/middleware/index.js
@@ -0,0 +1,59 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// all the middleare goes here
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               req.flash("error","Campground not found, Error!");
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error","You are not authorized to do that!");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error","You need to LoggedIn First");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               req.flash("error","Comment not found, Error!");
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error","You are not authorized to do that!");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error","You need to LoggedIn First");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error","You are currently logged out!! Please login.");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
